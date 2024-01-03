24f479c5154009b717db82e5501f3aa71fd936ee
AppacheAZ <marcoaz2001@outlook.com>
2024-01-03 19:11:27 +0100

    delete ft_strlcpy

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: marcoalv <marcoalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 18:23:26 by marcoalv          #+#    #+#             */
-/*   Updated: 2023/11/20 21:26:57 by marcoalv         ###   ########.fr       */
+/*   Updated: 2024/01/03 19:07:16 by marcoalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,4 @@ void			*ft_calloc(size_t count, size_t size);
 void			ft_bzero(void *s, size_t n);
 char			*ft_strjoin(char const *s1, char const *s2);
 unsigned long	ft_strlen(const char *s);
-size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 #endif
