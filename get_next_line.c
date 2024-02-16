6ef1e21cbb0905bc9c90c667ed0a296b6fe9ffcf
AppacheAZ <marcoaz2001@outlook.com>
2024-02-16 19:59:28 +0100

    new function to read the lines and define BUFFER_SIZE, emulate GNL but still must pass the tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marcoalv <marcoalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 18:23:00 by marcoalv          #+#    #+#             */
-/*   Updated: 2024/02/14 18:51:54 by marcoalv         ###   ########.fr       */
+/*   Updated: 2024/02/16 19:56:09 by marcoalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strdup(const char *s)
 	size_t	n;
 	size_t	i;
 	size_t	aux;
-	
+
 	i = 0;
 	n = ft_strlen(s);
 	ptr = (char *)malloc(n + 1 * sizeof(char));
@@ -43,30 +43,6 @@ char	*ft_strdup(const char *s)
 	return (ptr);
 }
 
-char	*ft_line(char *buffer)
-{
-	char	*point;
-	char 	*line;
-	int		i;
-	
-	i = 0;
-	if (!buffer)
-		return (NULL);
-	line = ft_calloc(ft_strlen(buffer) + 1, sizeof(char));
-	point = ft_strchr(buffer, '\n');
-	if (!point || !line)
-		return (NULL);
-	while (buffer[i] != '\0' || buffer[i] != '\n')
-	{
-		line[i] = buffer[i];
-		i++;
-	}
-	line[i] = '\n';
-	free(buffer);
-	return (line);
-	
-}
-
 char	*ft_next(char *buffer)
 {
 	char	*temp;
@@ -82,9 +58,34 @@ char	*ft_next(char *buffer)
 	i++;
 	j = 0;
 	while (buffer[i] && buffer[i] != '\n')
+	{
 		temp[j++] = buffer[i++];
+		if (buffer[i + 1] == '\0')
+			return (NULL);
+	}
 	free(buffer);
 	return (temp);
+}
+
+char	*ft_line(char *buffer, char *new_line)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	line = ft_calloc(ft_strlen(buffer) + 2, sizeof(char));
+	if (!line || !buffer || !buffer[0] || ft_strlen(buffer) == 0)
+		return (NULL);
+	while (buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	line[i] = buffer[i];
+	line[i + 1] = '\0';
+	free(buffer);
+	buffer = ft_strdup(new_line + 1);
+	return (line);
 }
 
 char	*read_file(int fd, char *buffer_prime)
@@ -107,7 +108,6 @@ char	*read_file(int fd, char *buffer_prime)
 		if (byte_read == 0)
 			break ;
 		buffer_read[byte_read] = '\0';
-		//buffer_prime = ft_free(buffer_prime, buffer_read);
 		buffer_prime = ft_strjoin(buffer_prime, buffer_read);
 		if (ft_strchr(buffer_prime, '\n'))
 			break ;
@@ -120,28 +120,21 @@ char	*get_next_line(int fd)
 {
 	static char		*buffer_gnl;
 	char			*line;
-	char			*temp;
+	char			*new_line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	buffer_gnl = read_file(fd, buffer_gnl);
 	if (!buffer_gnl || !*buffer_gnl)
 		return (NULL);
-	line = ft_strchr(buffer_gnl, '\n');
-	if (line)
+	new_line = ft_strchr(buffer_gnl, '\n');
+	if (new_line)
 	{
-		*line = '\0';
-		temp = ft_strdup(buffer_gnl);
-		free(buffer_gnl);
-		buffer_gnl = ft_strdup(line + 1);
-		return (temp);
+		line = ft_line(buffer_gnl, new_line);
+		return (line);
 	}
-	temp = ft_strdup(buffer_gnl);
+	line = ft_strdup(buffer_gnl);
 	free(buffer_gnl);
-	if (ft_strchr(temp, '\0'))
-		return (temp);
-	buffer_gnl = ft_next(buffer_gnl);
-	//ft_free(&buffer_gnl, ft_strlen(buffer_gnl));
-	//free(buffer_gnl);
-	return (temp);
+	buffer_gnl = ft_next(buffer_gnl); // buffer_gnl = NULL;
+	return (line);
 }
